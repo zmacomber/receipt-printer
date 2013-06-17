955f2988b31be8ca97afd82095c58b881048b2b5
zack.macomber <zack.macomber@gmail.com>
2013-06-17 22:48:50 +0000

    Initial upload of JUnit tests

---
+++ ReceiptPrinterTest/test/com/mbc/receiptprinter/util/ReceiptPrinterLoggerTest.java
@@ -0,0 +1,67 @@
+package com.mbc.receiptprinter.util;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.mbc.receiptprinter.constant.FilePaths;
+
+public class ReceiptPrinterLoggerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		clearLogFile();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		clearLogFile();
+	}
+	
+	static void clearLogFile() throws IOException {
+		File log = new File(FilePaths.LOG_PATH);
+		FileUtils.touch(log);
+		FileUtils.writeStringToFile(log, "");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+			
+	@Test
+	public void testGetFileHandler() throws Exception {
+		FileHandler fh = ReceiptPrinterLogger.getFileHandler();
+		assertNotNull(fh);
+	}
+			
+	@Test
+	public void testLogMessage() throws Exception {
+		ReceiptPrinterLogger.logMessage(this.getClass(), Level.SEVERE, "Testing the logging of a severe message...");
+	}
+	
+	@Test
+	public void testLogMessage_WithException() throws Exception {
+		Exception ex = new Exception("An issue has occurred...");
+		ReceiptPrinterLogger.logMessage(this.getClass(), Level.SEVERE, "Testing the logging of a severe message...", ex);
+	}
+	
+	@Test
+	public void testLogMessage_WithAllNullParms() throws Exception {
+		ReceiptPrinterLogger.logMessage(null, null, null, null);
+	}
+
+}
